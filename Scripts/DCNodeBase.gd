d0b2b60112fdcde588be8c7eb78dc324de3a8a12
mifth <paulgeraskin@gmail.com>
2023-12-29 21:31:55 +0100

    First commit

---
+++ Scripts/DCNodeBase.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _exit_tree():
+	queue_free()
+
+
+func _on_delete_text_button_pressed():
+	queue_free()
---
+++ Scripts/DCTextNode.gd
@@ -0,0 +1,11 @@
+extends GraphNode
+
+@export var text_node_text_resource: Resource
+
+
+func _exit_tree():
+	queue_free()
+
+func _on_add_text_button_pressed():
+	add_child(text_node_text_resource.instantiate())
+	set_slot( get_children().size() - 1, false, 0, Color.WHITE, true, 0, Color.WHITE)
